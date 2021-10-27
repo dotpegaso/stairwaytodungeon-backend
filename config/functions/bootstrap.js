7828f2b373c2e308349ca1fd655de2bd1dd9944c
dotpegaso <dotpegaso@gmail.com>
2021-10-27 02:52:21 -0300

    add socket

--- config/functions/bootstrap.js
+++ config/functions/bootstrap.js
@@ -1,4 +1,6 @@
-'use strict';
+"use strict";
+const socketIO = require("socket.io");
+const server = process.env.REACT_APP_SOCKET_ENDPOINT;
 
 /**
  * An asynchronous bootstrap function that runs before
@@ -7,7 +9,20 @@
  * This gives you an opportunity to set up your data model,
  * run jobs, or perform some special logic.
  *
- * See more details here: https://strapi.io/documentation/developer-docs/latest/setup-deployment-guides/configurations.html#bootstrap
+ * See more details here: https://strapi.io/documentation/v3.x/concepts/configurations.html#bootstrap
  */
 
-module.exports = () => {};
+module.exports = () => {
+  const io = socketIO(server, {
+    cors: {
+      origin: "*",
+    },
+  });
+
+  io.on("connection", (socket) => {
+    console.log("a user connected");
+    socket.on("disconnect", () => console.log("a user disconnected"));
+  });
+
+  strapi.io = io;
+};
