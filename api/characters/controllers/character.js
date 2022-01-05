7f870fba6b5ec66f1c75b1e47412f770d009bf0f
dotpegaso <dotpegaso@gmail.com>
2022-01-05 01:37:09 -0300

    update

--- api/characters/controllers/character.js
+++ api/characters/controllers/character.js
@@ -1,24 +1,9 @@
 "use strict";
-const { sanitizeEntity } = require("strapi-utils");
-const _ = require("lodash");
+// const { sanitizeEntity } = require("strapi-utils");
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {
-  update: async (ctx) => {
-    const { id } = ctx.params;
-    const entity = await strapi.services.character.update(
-      { id },
-      ctx.request.body
-    );
-
-    strapi.emitToAllUsers(
-      "characters",
-      JSON.stringify({ message: "characters updated" })
-    );
-    return sanitizeEntity(entity, { model: strapi.models.character });
-  },
-};
+module.exports = {};
